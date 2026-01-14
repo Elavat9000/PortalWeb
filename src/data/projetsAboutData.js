020494193ed1bd2039b9fe8d6d4acb70d80960b8
Elavat9000 <elavat.9000@gmail.com>
2026-01-14 16:41:05 -0600

    Cambios de link en la pagina Web

--- src/data/projetsAboutData.js
+++ src/data/projetsAboutData.js
@@ -9,26 +9,29 @@ export const proyectos = [
   {
     titulo: "App con Tkinter Python",
     imagen: imgNote.src,
-    tecnologias: ["React", "Tailwind", "Express", "MongoDB"],
-    codigo: "https://github.com/Lautaro-R-collins/Front-react-todo",
+    tecnologias: ["tkinter","threading","Selenium", "sys", "Python"],
+    codigo: "https://github.com/Elavat9000/Campanas_Selenium/tree/main",
+    demo: "https://youtu.be/1K3Yk8L6v8M",
   },
   {
     titulo: "Página de Remolques",
     imagen: imgRemolques.src,
     tecnologias: ["Astro", "Tailwind", "JavaScript"],
-    codigo: "https://github.com/tu-repo/remolques",
+    codigo: "https://github.com/Elavat9000/concaresa-trailers-hub",
+    demo: "https://concaresa-trailers-hub.vercel.app/",
   },
   {
     titulo: "Página de Remolques (Landing)",
     imagen: imgRemolquesAlt.src,
     tecnologias: ["HTML", "CSS", "JavaScript"],
-    codigo: "https://github.com/tu-repo/remolques-landing",
+    codigo: "https://github.com/Elavat9000/remolquesC",
   },
   {
     titulo: "Analisis de Datos",
     imagen: imgPortfolio.src,
     tecnologias: ["Astro", "React", "Tailwind"],
-    codigo: "https://github.com/tu-repo/portfolio",
+    codigo: "https://github.com/Elavat9000/Challenge-ONE-Data-Science-Telecom-X-parte-2",
+    demo: "https://github.com/Elavat9000/Challenge-ONE-Data-Science-Telecom-X-parte-2/blob/main/Telecom_X_parte_2.ipynb",
   },
 ];
 
